151af76e8d232059bbbc8a3a4f40c708ed2ee94b
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-08-13 03:58:20 +0530

    changed the log output to stderr, to enable piping

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/Muhammed-Rajab/file-boy/cmd"
+import (
+	"log"
+	"os"
+
+	"github.com/Muhammed-Rajab/file-boy/cmd"
+)
 
 func main() {
+	log.SetOutput(os.Stderr)
 	cmd.Execute()
 }
