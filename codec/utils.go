b67fc06a1ce888b0b775d939fb1fe4ef436b2675
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-08-13 02:56:39 +0530

    removed unwanted function

--- codec/utils.go
+++ codec/utils.go
@@ -9,14 +9,6 @@ import (
 	"golang.org/x/crypto/pbkdf2"
 )
 
-func fileExists(filePath string) bool {
-	_, err := os.Stat(filePath)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return err == nil
-}
-
 func directoryExists(dirPath string) (bool, error) {
 	info, err := os.Stat(dirPath)
 	if os.IsNotExist(err) {
